374a9f91203418753af3cd2af95a2bde5e1d81a7
Paul Park <2000mageia@gmail.com>
2024-08-25 02:19:04 +0900

    feat: Add lazy loading for images and optimize page load

--- zzk/lib/pages/orderPage/order.dart
+++ zzk/lib/pages/orderPage/order.dart
@@ -68,43 +68,49 @@ class _OrderPageState extends State<OrderPage> {
                     bottom: lerpDouble(10, 60, sqrt(_opacity))!,
                   ),
                   centerTitle: true,
-                  title: Stack(
-                    clipBehavior: Clip.none,
-                    children: [
-                      Positioned(
-                        left: -8 * 2,
-                        right: -8 * 2,
-                        top: -4 * 2,
-                        bottom: -4 * 2,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color.lerp(
-                              Theme.of(context).appBarTheme.backgroundColor,
-                              Colors.white,
-                              lerpDouble(0, 1, sqrt(_opacity))!,
-                            ),
-                            borderRadius: BorderRadius.circular(0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.5 * _opacity * _opacity),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: Offset(0, 3),
+                  title: snapshot.connectionState == ConnectionState.waiting
+                      ? CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Positioned(
+                              left: -8 * 2,
+                              right: -8 * 2,
+                              top: -4 * 2,
+                              bottom: -4 * 2,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color.lerp(
+                                    Theme.of(context).appBarTheme.backgroundColor,
+                                    Colors.white,
+                                    lerpDouble(0, 1, sqrt(_opacity))!,
+                                  ),
+                                  borderRadius: BorderRadius.circular(0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.5 * _opacity * _opacity),
+                                      spreadRadius: 5,
+                                      blurRadius: 7,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ],
-                          ),
+                            ),
+                            Container(
+                              child: Text(
+                                snapshot.data.restaurantName,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Text(
-                        '레스토랑 한국점',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
                   background: Image.network(
                     'https://www.eatright.org/-/media/images/eatright-landing-pages/foodgroupslp_804x482.jpg?as=0&w=967&rev=d0d1ce321d944bbe82024fff81c938e7&hash=E6474C8EFC5BE5F0DA9C32D4A797D10D',
                     fit: BoxFit.cover,
